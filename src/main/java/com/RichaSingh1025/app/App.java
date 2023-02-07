f137cab9354215b106871513952f3028481419f0
Praveen <praveenlsingh@gmail.com>
2023-02-07 23:45:03 +0530

    Initial commit

---
+++ src/main/java/com/RichaSingh1025/app/App.java
@@ -0,0 +1,13 @@
+package com.RichaSingh1025.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
